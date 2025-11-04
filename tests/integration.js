54049fc725c5d7b3e895bdc5cec1b4348c544d52
d-makarov-d <danila.makarov@gmail.com>
2025-11-04 16:42:58 +0300

    Add todo

--- tests/integration.js
+++ tests/integration.js
@@ -1,6 +1,8 @@
 import test from 'node:test';
 import assert from 'assert';
 
+// TODO: server must be launched automatically in tests initialization. Think about lounching a specific docker container
+
 test('server status test', async (t) => {
     const response = await fetch('http://localhost:3000/status', {
         method: 'GET',
